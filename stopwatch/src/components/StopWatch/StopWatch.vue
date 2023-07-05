52569fa14dba67256246b337e591d76251d8ad14
seregaalex <xerox007>
2023-07-05 11:25:23 +0300

    Добавил коменты

--- stopwatch/src/components/StopWatch/StopWatch.vue
+++ stopwatch/src/components/StopWatch/StopWatch.vue
@@ -6,19 +6,26 @@
       {{ formatTime }}
     </div>
     <div class="btn-block">
-      <div v-on:click="start" v-show="!isActive && isStopped">
-        <img src="img/start.png" alt="start" />
+<!--      желательно использовать v-if а еще лучше вообще не использовать отдельный элемент, просто менять иконку,
+и по клику менять стейт isStopped и прочее типа того из за v-show еще промаргивание есть-->
+      <div v-on:click="startStop">
+        <img :src="isActive ? 'img/active-pause.png' : 'img/start.png'" alt="start" />
       </div>
+<!--      <div v-on:click="start" v-show="!isActive">-->
+<!--        <img src="img/start.png" alt="start" />-->
+<!--      </div>-->
 
-      <div v-on:click="stop" v-show="isActive">
-        <img src="img/active-pause.png" alt="stop" class="stop" v-if="isActive" />
-        <img src="img/pause.png" alt="stop" class="stop" v-else />
-      </div>
-
+<!--      <div v-on:click="stop" v-show="isActive">-->
+<!--        <img src="img/active-pause.png" alt="stop" class="stop" v-if="isActive" />-->
+<!--        <img src="img/pause.png" alt="stop" class="stop" v-else />-->
+<!--      </div>-->
       <div v-on:click="reset">
-        <img src="img/active-stop.png" alt="reset" class="reset" v-if="isActive" />
-        <img src="img/stop.png" alt="reset" class="reset" v-else />
+        <img :src="isActive ? 'img/active-stop.png' : 'img/stop.png'" alt="start" />
       </div>
+<!--      <div v-on:click="reset">-->
+<!--        <img src="img/active-stop.png" alt="reset" class="reset" v-if="isActive" />-->
+<!--        <img src="img/stop.png" alt="reset" class="reset" v-else />-->
+<!--      </div>-->
     </div>
   </div>
 </template>
@@ -45,11 +52,14 @@ export default {
       let seconds = Math.floor(this.elapsedTime / 1000) % 60;
       let minutes = Math.floor(this.elapsedTime / 1000 / 60) % 60;
       let hours = Math.floor(this.elapsedTime / 1000 / 60 / 60);
-      
+
       return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`;
     },
   },
-  methods: {
+  methods: {      // в код не лез, на мой взгляд слишком сложно, надо было использовать что то вроде moment.js и работать с объектом даты
+    startStop() {
+      this.isActive ? this.stop() : this.start()
+    },
     start() {
       // if (this.interval) return;
       // this.interval = setInterval(() => {
